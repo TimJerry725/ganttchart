187359b27f0cd081d236ead92948692621f517e1
Timothy025 <timothy025@gmail.com>
2026-01-15 15:16:35 +0530

    chore: prepare iris-gantt for npm publish (lib build + metadata)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'node:path'
 
-// https://vite.dev/config/
+// Library + Storybook / demo build config
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/components/index.ts'),
+      name: 'IrisGantt',
+      fileName: 'iris-gantt',
+      formats: ['es', 'umd'],
+    },
+    rollupOptions: {
+      // Externalize peer deps for library build
+      external: [
+        'react',
+        'react-dom',
+        'antd',
+        '@fortawesome/react-fontawesome',
+        '@fortawesome/fontawesome-svg-core',
+        '@fortawesome/free-solid-svg-icons',
+      ],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          antd: 'antd',
+          '@fortawesome/react-fontawesome': 'ReactFontawesome',
+          '@fortawesome/fontawesome-svg-core': 'FontAwesomeCore',
+          '@fortawesome/free-solid-svg-icons': 'FontAwesomeSolid',
+        },
+      },
+    },
+  },
 })
